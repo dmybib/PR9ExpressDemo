372d706ce90505fc74b9ea6b824bfb6e881c98e7
dmybib <gbbcof@gmail.com>
2024-05-24 11:32:21 +0300

    added into the repo

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const productsRouter = require('./routes/products')
+const logEvent = require('./middlewares/logEvents');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use((req, res, next) => {
+    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}\n`, 'requestTracing.txt');
+    console.log(`${req.method}\t${req.path}`);
+    next();
+});
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname});
+});
+
+app.get('/about(.html)?', (req, res) => {
+    //throw(new Error('Something went wrong'));
+    res.sendFile('./views/about.html', { root: __dirname});
+});
+
+app.get('/products(.html)?', (req, res) => {
+    res.sendFile('./views/products.html', { root: __dirname});
+});
+
+app.use('/api/products', productsRouter);
+
+app.get('/error', (req, res, next) => {
+    const x = 5 / 0;
+    next();
+});
+
+app.use(require('./middlewares/errorHandler'));
+
+app.listen(PORT, () => {
+    logEvent(`Server starts listening on port ${PORT}`, 'ServerEvents.txt');
+    console.log(`Server starts listening on port ${PORT}`);
+});
